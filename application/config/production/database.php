69bdfbb9b97b3fc40ce6de20c9f1535592b5d82c
Aldison Diego <aldsondiego@hotmail.com>
2015-02-23 11:13:42 -0400

    ver error_reporting

--- application/config/production/database.php
+++ application/config/production/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'puong531_geral';
+$db['default']['password'] = 'cyberGERAL@3021';
+$db['default']['database'] = 'puong531_anw';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
